25535cc3cdde8cfeb7830b671b3f7310257f1167
Kelvin-SG <kalvin10.kc@gmail.com>
2022-06-06 21:28:32 -0300

    Substituição da virgula por -

---
+++ modeling.py
@@ -0,0 +1,18 @@
+#Biblioteca para trabalhar com DataFrame
+import pandas as pd
+
+#Lendo/Abrindo o arquivo .csv
+covid = pd.read_csv('/home/virtual/Desktop/Exercicio_Hbase/covid/covid_19_data.csv', sep=',')
+
+#apagando todos regitros com valores nullos
+covid19 = covid.dropna()
+
+#susbtituir virgula na coluna Province/State por ' -' 
+covid19['Province/State'].replace(',', ' -', regex=True, inplace=True) # regex=True para fazer a alteração dentro da string
+
+#exportando em um novo arquivo .csv
+covid19.to_csv('/home/virtual/Desktop/Exercicio_Hbase/covid/covid19.csv', index=False, sep=',')
+
+#Proximos passos:
+#Commitar e enviar para o git;
+#Baixar na maquina Cloudera
